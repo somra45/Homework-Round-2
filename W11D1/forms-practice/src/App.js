f4d4d7e7deac155ddfe20adef24f90fee1eb4d52
root <somra45@gmail.com>
2023-07-10 18:58:55 -0400

    homework

---
+++ W11D1/forms-practice/src/App.js
@@ -0,0 +1,11 @@
+import ContactUs from './ContactUs'
+
+function App() {
+  return (
+    <>
+      <ContactUs />
+    </>
+  );
+}
+
+export default App;
---
+++ W11D1/forms-practice/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+
+function Root(props) {
+  return (
+    <BrowserRouter> 
+      <App /> 
+    </BrowserRouter> 
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render( 
+  <Root/>
+);
+
